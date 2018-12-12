4368d3aee2d476a2b405d80529d41fa5b08557a4
Myeongjae Kim <dev@myeongjae.kim>
2018-12-12 14:17:49 +0000

    Daily commit

--- commit.sh
+++ commit.sh
@@ -4,14 +4,12 @@ echo "$(date)" >> log
 git add -A
 git commit -m "Daily commit"
 
-echo "$PW"
-
 expect <<EOF
 set timeout 3
 spawn git push
 expect "Username for 'https://github.com':"
-	send "$USER\r"
+	send "$__GIT_USER__\r"
 expect "Password for 'https://hrzon@github.com':"
-	send "$PW\r"
+	send "$__GIT_PW__\r"
 expect eof
 EOF
