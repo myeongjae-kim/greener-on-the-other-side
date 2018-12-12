d8a0ed0d513b6852ba44e86c4522cab0a4d8d024
Myeongjae Kim <dev@myeongjae.kim>
2018-12-12 14:15:27 +0000

    Daily commit

---
+++ commit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# $USER and $PW must be declared out of this script
+echo "$(date)" >> log
+git add -A
+git commit -m "Daily commit"
+
+expect <<EOF
+set timeout 3
+spawn git push
+expect "Username for 'https://github.com':"
+	send "$USER\r"
+expect "Password for 'https://hrzon@github.com':"
+	send "$PW\r"
+expect eof
+EOF
